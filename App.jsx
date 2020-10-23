b0ae8ad8cc884d3acbcad90d0d9d167b4c23985c
Thomas <thomas@craftacademy.se>
2020-10-23 11:10:11 +0200

    configures metro to resolve jsx suffix renames comopnent files

--- App.jsx
+++ App.jsx
@@ -1,25 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import MainScreen from "./src/components/MainScreen";
 import SecondScreen from "./src/components/SecondScreen";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-// import { useSelector } from "react-redux";
 
 const Stack = createStackNavigator()
 
 const App = () => {
-  // const greeting = useSelector(state => state.mainPageGreeting)
   return (
-    // <View style={styles.container}>
-    //   <Text>{greeting}</Text>
-    //   <StatusBar style="auto" />
-    // </View>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="The First Page" component={MainScreen} />
-        <Stack.Screen name="Another page" component={SecondScreen}/>
+        <Stack.Screen name="Another page" component={SecondScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
